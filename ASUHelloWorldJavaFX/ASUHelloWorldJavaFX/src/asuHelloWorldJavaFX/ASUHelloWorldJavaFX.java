58e70bc8140c7697e081bfa7e414bc4eb6c202b2
cadon <cadon@DESKTOP-MBI36Q4.3521.dhcp.asu.edu>
2024-09-24 15:41:44 -0700

    1st commit and push

---
+++ ASUHelloWorldJavaFX/ASUHelloWorldJavaFX/src/asuHelloWorldJavaFX/ASUHelloWorldJavaFX.java
@@ -0,0 +1,33 @@
+package asuHelloWorldJavaFX;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+ 
+public class ASUHelloWorldJavaFX extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    public void start(Stage primaryStage) {
+    	System.out.println("Cadon Duong Hello World!");
+    	System.out.println("It started!");
+        primaryStage.setTitle("Cadon Duong Spring 2024");
+        Button btn = new Button();
+        btn.setText("Display: 'Cadon Duong says: Hello World!'");
+        btn.setOnAction(new EventHandler<>() {
+            public void handle(ActionEvent event) {
+                System.out.println("Cadon Duong: Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+}
